4e9b996a2c6f269a2f07a319933b264816d2ef83
Andrey Zavodov <andrey.zavodov@jetbrains.com>
2021-01-27 19:22:20 +0300

    Add header with settings

---
+++ settings.h
@@ -0,0 +1,11 @@
+#ifndef ITMO_SPO_2021_03_SETTINGS_H
+    //include guard
+    #define ITMO_SPO_2021_03_SETTINGS_H
+    //general
+    #define PORT 3333
+    //server
+    #define QUEUE_LIMIT 100
+    #define MAX_LENGTH 4096
+    //client
+    #define MAX_LENGTH 4096
+#endif
