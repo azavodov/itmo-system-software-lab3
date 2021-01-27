4c7f5b56c970e422b8cbaf18735a00da6829e381
Andrey Zavodov <andrey.zavodov@jetbrains.com>
2021-01-27 19:22:43 +0300

    Add 'server' and 'client' implementation

--- client.c
+++ client.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "settings.h"
+
 
 int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
+    if (argc < 3) {
+        return -1;
+    }
 
+    char client_message[MAX_LENGTH], server_message[MAX_LENGTH];
+    int tcp_client_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in socket_address;
+    memset(&socket_address, 0, sizeof socket_address);
+    socket_address.sin_family = AF_INET;
+    socket_address.sin_port = htons(PORT);
+    inet_pton(AF_INET, argv[1], &socket_address.sin_addr);
+    connect(tcp_client_socket_descriptor, (struct sockaddr *) &socket_address, sizeof socket_address);
+
+    for (int i = 2; i < argc; ++i) {
+        memset(client_message, 0, MAX_LENGTH);
+        memset(server_message, 0, MAX_LENGTH);
+        printf(" --- Path: %s\n", argv[i]);
+        strcat(client_message, argv[i]);
+        strcat(client_message, "\n");
+        write(tcp_client_socket_descriptor, client_message, strlen(client_message));
+        read(tcp_client_socket_descriptor, server_message, MAX_LENGTH);
+        printf("%s", server_message);
+        printf(" --- --- ---\n");
+    }
+
+    memset(client_message, 0, MAX_LENGTH);
+    strcat(client_message, "client: exit\n");
+    write(tcp_client_socket_descriptor, client_message, strlen(client_message));
     return 0;
 }
--- server.c
+++ server.c
@@ -1,8 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <signal.h>
+#include <pthread.h>
+#include "settings.h"
+
+int tcp_server_socket_descriptor;
+
+void *client_request(void *p_client_socket_descriptor) {
+    int client_socket_descriptor = *((int *) p_client_socket_descriptor);
+    free(p_client_socket_descriptor);
+    uint8_t client_message[MAX_LENGTH + 1], server_message[MAX_LENGTH + 1];
+    memset(client_message, 0, MAX_LENGTH);
+    memset(server_message, 0, MAX_LENGTH);
+    int message_length;
+    while (1) {
+        memset(client_message, 0, MAX_LENGTH);
+        while ((message_length = read(client_socket_descriptor, client_message, MAX_LENGTH))
+               > 0) {
+
+            if (client_message[message_length - 1] == '\n') break;
+        }
+        client_message[message_length - 1] = 0;
+        if (message_length - 2 >= 0 && client_message[message_length - 2] == '\r')
+            client_message[message_length - 2] = 0;
+        if (strcmp((const char *) client_message, "exit") == 0) {
+            close(client_socket_descriptor);
+            return NULL;
+        }
+        printf("server: client request: %s\n", client_message);
+        struct dirent *file;
+        DIR *directory = opendir((const char *) client_message);
+        if (!directory) {
+            char *s = "server: path does not exist\n";
+            write(client_socket_descriptor, s, strlen(s));
+        } else {
+            memset(server_message, 0, MAX_LENGTH);
+            while ((file = readdir(directory)) != NULL) {
+                strcat((char *) server_message, file->d_name);
+                strcat((char *) server_message, "\n");
+            }
+            closedir(directory);
+            write(client_socket_descriptor, (char *) server_message, strlen((char *) server_message));
+        }
+    }
+}
+
 
 int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
-    return 0;
+    (void) argc; (void) argv;
+
+    struct sockaddr_in socket_address;
+    memset(&socket_address, 0, sizeof socket_address);
+    socket_address.sin_family = AF_INET;
+    socket_address.sin_port = htons(PORT);
+    socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    tcp_server_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    bind(tcp_server_socket_descriptor, (struct sockaddr *) &socket_address, sizeof(socket_address));
+    listen(tcp_server_socket_descriptor, QUEUE_LIMIT);
+
+    printf("server: waiting for connections ...\n");
+    while (1) {
+        int *tcp_client_socket_descriptor = malloc(sizeof(int));
+        *tcp_client_socket_descriptor = accept(tcp_server_socket_descriptor, (struct sockaddr *) NULL, NULL);
+        pthread_t connection;
+        pthread_create(&connection, NULL, client_request, tcp_client_socket_descriptor);
+    }
+
 }
